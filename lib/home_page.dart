31b766e9fc01e1bf928f7641feca24d843f70bf9
Jaque Faino <jaquecavaller@gmail.com>
2024-09-06 10:04:03 -0300

    - Entering the process of decreasing some raster janks; - Probably is related to the poorly done state management and too many nested widgets making a complex widget tree.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -130,221 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: Container(
-        child: switch (_viewType) {
-          WEEK => WeekView(
-            initialDay: initialDay,
-            minuteSlotSize: minuteSlot,
-            weekNumberBuilder: (date) => const Text(''),
-            hourIndicatorSettings: hourIndicatorSettings,
-            timeLineStringBuilder: (date, {secondaryDate}) =>
-                timeLineString(date),
-            halfHourIndicatorSettings: halfHourSettings,
-            showHalfHours: showHalfHour,
-            showQuarterHours: showQuarterHour,
-            weekPageHeaderBuilder: (startDate, endDate) => Container(
-              color: Theme.of(context).colorScheme.surface,
-              child: TextButton(
-                onPressed: () {
-                  showDatePicker(
-                      context: context,
-                      firstDate: DateTime(2020),
-                      lastDate: DateTime(2030),
-                      initialDate: startDate,
-                      helpText: 'Selecione uma data',
-                      cancelText: 'Cancelar',
-                      confirmText: 'Confirmar',
-                      fieldLabelText: 'Data',
-                      fieldHintText: 'dd/mm/aa',
-                      errorFormatText: 'Formato de data inválido',
-                      errorInvalidText: 'Data inválida',
-                      builder: (context, child) {
-                        return Theme(
-                          data: Theme.of(context),
-                          child: child!,
-                        );
-                      }).then((picked) => {
-                    setState(() {
-                      initialDay = picked ?? initialDay;
-                    })
-                  });
-                },
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.calendar_today_outlined,
-                          // color: Theme.of(context).colorScheme.onSurface,
-                          // color: Colors.black,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            '${monthTitles[startDate.month - 1]} ${startDate.year}',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.onSurface,
-                              // fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // ---------------------------------------------------------------
-            // quarto de hora ta mostrando errado por algum motivo misterioso
-            // ---------------------------------------------------------------
-            // quarterHourIndicatorSettings:
-            //     const HourIndicatorSettings(color: Colors.grey, height: 0.5),
-
-            // -------------- UM MODO DE MOSTRAR TRES DIAS NA SEMANA!!!! -------
-            // startDay: WeekDays.monday,
-            // weekDays: [WeekDays.monday, WeekDays.tuesday, WeekDays.wednesday],
-            // weekPageHeaderBuilder: WeekHeader.hidden,
-            weekDayBuilder: (DateTime day) => Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.zero,
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Column(
-                children: [
-                  Text(
-                    weekTitles[day.weekday - 1],
-                    style: const TextStyle(
-                        // color: Colors.black,
-                        fontSize: 8),
-                  ),
-                  Text(
-                    day.day.toString(),
-                    style: const TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            controller: controller,
-            heightPerMinute: heightPerMinute,
-            eventTileBuilder:
-                (date, events, boundary, startDuration, endDuration) =>
-                customEventTileBuilder(
-                    date, events, boundary, startDuration, endDuration),
-            startHour: startHour,
-            endHour: endHour,
-            onEventTap: (events, date) => navigateToAppointmentRegistration(
-                context,
-                events[0].startTime!,
-                controller,
-                events[0] as CustomEvent),
-            onDateTap: (date) => navigateToAppointmentRegistration(
-                context,
-                date,
-                controller,
-                null),
-          ),
-          DAY => DayView(
-            initialDay: initialDay,
-            showHalfHours: showHalfHour,
-            showQuarterHours: showQuarterHour,
-            halfHourIndicatorSettings: halfHourSettings,
-            hourIndicatorSettings: hourIndicatorSettings,
-            minuteSlotSize: minuteSlot,
-            controller: controller,
-            heightPerMinute: heightPerMinute,
-            startHour: startHour,
-            endHour: endHour,
-            eventTileBuilder:
-                (date, events, boundary, startDuration, endDuration) =>
-                customEventTileBuilder(
-                    date, events, boundary, startDuration, endDuration),
-            timeStringBuilder: (date, {secondaryDate}) =>
-                timeLineString(date),
-            dayTitleBuilder: (date) => Container(
-              color: Colors.white,
-              child: TextButton(
-                onPressed: () {
-                  showDatePicker(
-                      context: context,
-                      firstDate: DateTime(2020),
-                      lastDate: DateTime(2030),
-                      initialDate: date,
-                      helpText: 'Selecione uma data',
-                      cancelText: 'Cancelar',
-                      confirmText: 'Confirmar',
-                      fieldLabelText: 'Data',
-                      fieldHintText: 'dd/mm/aa',
-                      errorFormatText: 'Formato de data inválido',
-                      errorInvalidText: 'Data inválida',
-                      builder: (context, child) {
-                        return Theme(
-                          data: ThemeData.light().copyWith(
-                            colorScheme: const ColorScheme.light(
-                              primary: Color.fromARGB(255, 2, 159, 243),
-                              onPrimary: Colors.white,
-                            ),
-                            dialogBackgroundColor: Colors.white,
-                          ),
-                          child: child!,
-                        );
-                      }).then((picked) => {
-                    setState(() {
-                      initialDay = picked ?? initialDay; // para nao ter que deixar
-                                                         // esse gerenciamento de estado aqui,
-                                                         // usar BLoC
-                    })
-                  });
-                },
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.calendar_today_outlined,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            '${monthTitles[date.month - 1]} ${date.year}',
-                            style: const TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          weekDaysTitles[date.weekday - 1],
-                          style: const TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                        Text(
-                          '${date.day}',
-                          style: const TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            onDateTap: (date) =>
-                navigateToAppointmentRegistration(context, date, controller, null),
-          ),
-          MONTH => MonthView(
-            controller: controller,
-            onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
-            onCellTap: (events, date) =>
-                navigateToAppointmentRegistration(context, date, controller, events[0] as CustomEvent),
-          ),
-          _ => throw Exception('Invalid view type: $_viewType'),
-        },
+        child: calendarViewType(context),
       ),
       bottomNavigationBar: NavigationBar(destinations: <Widget>[
         const NavigationDestination(
@@ -366,5 +152,223 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ]),
     );
+  }
+
+  StatefulWidget calendarViewType(BuildContext context) {
+    return switch (_viewType) {
+        WEEK => WeekView(
+          initialDay: initialDay,
+          minuteSlotSize: minuteSlot,
+          weekNumberBuilder: (date) => const Text(''),
+          hourIndicatorSettings: hourIndicatorSettings,
+          timeLineStringBuilder: (date, {secondaryDate}) =>
+              timeLineString(date),
+          halfHourIndicatorSettings: halfHourSettings,
+          showHalfHours: showHalfHour,
+          showQuarterHours: showQuarterHour,
+          weekPageHeaderBuilder: (startDate, endDate) => Container(
+            color: Theme.of(context).colorScheme.surface,
+            child: TextButton(
+              onPressed: () {
+                showDatePicker(
+                    context: context,
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2030),
+                    initialDate: startDate,
+                    helpText: 'Selecione uma data',
+                    cancelText: 'Cancelar',
+                    confirmText: 'Confirmar',
+                    fieldLabelText: 'Data',
+                    fieldHintText: 'dd/mm/aa',
+                    errorFormatText: 'Formato de data inválido',
+                    errorInvalidText: 'Data inválida',
+                    builder: (context, child) {
+                      return Theme(
+                        data: Theme.of(context),
+                        child: child!,
+                      );
+                    }).then((picked) => {
+                  setState(() {
+                    initialDay = picked ?? initialDay;
+                  })
+                });
+              },
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.calendar_today_outlined,
+                        // color: Theme.of(context).colorScheme.onSurface,
+                        // color: Colors.black,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '${monthTitles[startDate.month - 1]} ${startDate.year}',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onSurface,
+                            // fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // ---------------------------------------------------------------
+          // quarto de hora ta mostrando errado por algum motivo misterioso
+          // ---------------------------------------------------------------
+          // quarterHourIndicatorSettings:
+          //     const HourIndicatorSettings(color: Colors.grey, height: 0.5),
+
+          // -------------- UM MODO DE MOSTRAR TRES DIAS NA SEMANA!!!! -------
+          // startDay: WeekDays.monday,
+          // weekDays: [WeekDays.monday, WeekDays.tuesday, WeekDays.wednesday],
+          // weekPageHeaderBuilder: WeekHeader.hidden,
+          weekDayBuilder: (DateTime day) => Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.zero,
+            padding: const EdgeInsets.symmetric(vertical: 5),
+            child: Column(
+              children: [
+                Text(
+                  weekTitles[day.weekday - 1],
+                  style: const TextStyle(
+                      // color: Colors.black,
+                      fontSize: 8),
+                ),
+                Text(
+                  day.day.toString(),
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          controller: controller,
+          heightPerMinute: heightPerMinute,
+          eventTileBuilder:
+              (date, events, boundary, startDuration, endDuration) =>
+              customEventTileBuilder(
+                  date, events, boundary, startDuration, endDuration),
+          startHour: startHour,
+          endHour: endHour,
+          onEventTap: (events, date) => navigateToAppointmentRegistration(
+              context,
+              events[0].startTime!,
+              controller,
+              events[0] as CustomEvent),
+          onDateTap: (date) => navigateToAppointmentRegistration(
+              context,
+              date,
+              controller,
+              null),
+        ),
+        DAY => DayView(
+          initialDay: initialDay,
+          showHalfHours: showHalfHour,
+          showQuarterHours: showQuarterHour,
+          halfHourIndicatorSettings: halfHourSettings,
+          hourIndicatorSettings: hourIndicatorSettings,
+          minuteSlotSize: minuteSlot,
+          controller: controller,
+          heightPerMinute: heightPerMinute,
+          startHour: startHour,
+          endHour: endHour,
+          eventTileBuilder:
+              (date, events, boundary, startDuration, endDuration) =>
+              customEventTileBuilder(
+                  date, events, boundary, startDuration, endDuration),
+          timeStringBuilder: (date, {secondaryDate}) =>
+              timeLineString(date),
+          dayTitleBuilder: (date) => Container(
+            color: Colors.white,
+            child: TextButton(
+              onPressed: () {
+                showDatePicker(
+                    context: context,
+                    firstDate: DateTime(2020),
+                    lastDate: DateTime(2030),
+                    initialDate: date,
+                    helpText: 'Selecione uma data',
+                    cancelText: 'Cancelar',
+                    confirmText: 'Confirmar',
+                    fieldLabelText: 'Data',
+                    fieldHintText: 'dd/mm/aa',
+                    errorFormatText: 'Formato de data inválido',
+                    errorInvalidText: 'Data inválida',
+                    builder: (context, child) {
+                      return Theme(
+                        data: ThemeData.light().copyWith(
+                          colorScheme: const ColorScheme.light(
+                            primary: Color.fromARGB(255, 2, 159, 243),
+                            onPrimary: Colors.white,
+                          ),
+                          dialogBackgroundColor: Colors.white,
+                        ),
+                        child: child!,
+                      );
+                    }).then((picked) => {
+                  setState(() {
+                    initialDay = picked ?? initialDay; // para nao ter que deixar
+                                                       // esse gerenciamento de estado aqui,
+                                                       // usar BLoC
+                  })
+                });
+              },
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.calendar_today_outlined,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '${monthTitles[date.month - 1]} ${date.year}',
+                          style: const TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        weekDaysTitles[date.weekday - 1],
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        '${date.day}',
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          onDateTap: (date) =>
+              navigateToAppointmentRegistration(context, date, controller, null),
+        ),
+        MONTH => MonthView(
+          controller: controller,
+          onPageChange: (date, pageIndex) => print("$date, $pageIndex"),
+          onCellTap: (events, date) =>
+              navigateToAppointmentRegistration(context, date, controller, events[0] as CustomEvent),
+        ),
+        _ => throw Exception('Invalid view type: $_viewType'),
+      };
   }
 }
